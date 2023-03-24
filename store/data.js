53713ef03750387ddb1c04094d5d7cdab058d21f
Rooshi <rushisat11@gmail.com>
2023-03-24 19:49:10 +0530

    Update data.js

--- store/data.js
+++ store/data.js
@@ -1,8 +1,8 @@
 // A collection of musicians where the key is the unique id
 module.exports = {
   coltrane: {
-    firstName: 'John',
-    lastName: 'Coltrane',
+    firstName: 'Karry',
+    lastName: 'Mark',
     genre: 'JAZZ',
   },
 
